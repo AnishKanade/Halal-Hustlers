5192bc0ef0991fdfe1dbec9dec87e18af5f59bb3
talhamirza <talha.mirza001@umb.edu>
2024-05-12 12:36:49 -0400

    first commit

---
+++ Assets/Crossy Road/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int coinValue = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player picked up a coin");
+            Manager.instance.UpdateCoinCount(coinValue);
+            Destroy(this.gameObject);
+        }
+    }
+}
